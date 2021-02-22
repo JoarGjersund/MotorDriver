eda8acfd4e65a7c85859d7e15d7215be40a42e75
Joar andreas Gjersund <joarandreasgjersund@Joars-MacBook-Air.local>
2021-02-22 13:40:30 +0100

    working solution.

---
+++ src/Motordriver.cpp
@@ -0,0 +1,107 @@
+#include <Motordriver.h>
+
+
+MotorDriver::MotorDriver(int pin_enable, int pin_phase){
+
+    PIN_ENABLE=pin_enable;
+    PIN_PHASE=pin_phase;
+
+    pinMode(PIN_ENABLE, OUTPUT);
+    pinMode(PIN_PHASE, OUTPUT);
+
+
+}
+int MotorDriver::getCurrentPosition(){
+    return currentPosition;
+}
+
+void MotorDriver::recalibrate(int calibrationdirection, int calibrationspeed, int calibrationconstant_new, int percent_delay){
+
+    digitalWrite(PIN_PHASE, calibrationdirection);
+    analogWrite(PIN_ENABLE, calibrationspeed);
+
+    if (calibrationdirection==0) currentPosition=angle_min;
+    else currentPosition=angle_max;
+
+    if (calibrationconstant_new>0) delay(percent_delay*(1/100)*calibrationconstant_new*(255/speed));
+    else delay(percent_delay*(1/100)*calibrationConstant*(255/speed));
+
+    timeWhenLastCalibrated = millis();
+}
+
+
+
+void MotorDriver::setTargetPosition(int target){
+    targetPosition=target;
+
+}
+
+
+
+
+
+
+
+int MotorDriver::update(){
+
+
+    static bool init = false;
+    if (!init){
+        timeWhenLastUpdated=millis();
+        init=true;
+    }
+
+    int timeSinceLastUpdated=millis()-timeWhenLastUpdated;
+    
+
+
+    int degreesMovedSinceLastUpdate=timeSinceLastUpdated*180/(calibrationConstant);
+
+    if (degreesMovedSinceLastUpdate>0) timeWhenLastUpdated=millis();
+
+    currentPosition+=degreesMovedSinceLastUpdate*direction;
+
+    int targetPositionRounded=round(targetPosition/minimumStepSize)*minimumStepSize;
+
+        // decide direction to move.
+    if ( targetPositionRounded > currentPosition && currentPosition < angle_max){
+        direction=1;
+        digitalWrite(PIN_PHASE, 255); 
+    } else if ( targetPositionRounded < currentPosition && currentPosition > angle_min){
+        direction=-1;
+        digitalWrite(PIN_PHASE, 0);
+    }
+
+    if (abs(currentPosition-targetPositionRounded) <  minimumStepSize )
+    {
+        direction=0;
+        analogWrite(PIN_ENABLE, 0);
+    }else{
+        analogWrite(PIN_ENABLE, speed);
+    }
+
+    
+
+
+    
+    if (millis()-timeWhenLastCalibrated >10000){
+        if (currentPosition>(2*angle_max-angle_min)/3) recalibrate(0, 255, calibrationConstant, 10);
+        else if (currentPosition<(angle_max-angle_min)/3) recalibrate(255, 255, calibrationConstant, 10);        
+    } else if (millis()-timeWhenLastCalibrated >60000){
+        if (currentPosition>(5*angle_max-angle_min)/6) recalibrate(0, 255, calibrationConstant, 10);
+        else if (currentPosition<5*(angle_max-angle_min)/12) recalibrate(255, 255, calibrationConstant, 10);        
+    } else if (millis()-timeWhenLastCalibrated >120000){
+        if (currentPosition>(angle_max-angle_min)/2) recalibrate(0, 255, calibrationConstant, 10);
+        else recalibrate(255, 255, calibrationConstant, 10);        
+    } 
+    
+
+    return currentPosition;
+
+
+
+    }
+
+
+
+
--- src/Motordriver.h
+++ src/Motordriver.h
@@ -1,17 +1,27 @@
+#include <Arduino.h>
+#ifdef ESP32
+#include <analogWrite.h>
+#endif
 class MotorDriver {
     public:
     MotorDriver(int PIN_ENABLE, int PIN_PHASE);
-    void update();
+    int update();
     void setTargetPosition(int target);
     int getCurrentPosition();
     int targetPosition;
-    int calibrationConstant=200; // slower motor -> Higher number
+    int speed = 255;
+    void recalibrate(int direction = 0, int speed = 255, int calibrationconstant_new = 0, int percent_delay = 100);
 
     private:
-    int currentPositon;
-    int speed;
-    int timeWhenLastUpdated;
+    int currentPosition = 0;
+    unsigned long timeWhenLastUpdated = 0;
+    unsigned long timeWhenLastCalibrated = 0;
     int PIN_ENABLE;
     int PIN_PHASE;
+    int direction = 0;
+    int calibrationConstant=130; // slower motor -> Higher number. ms needed to go from 0 to 180 at maximum speed.
+    int minimumStepSize=2; // minimum step size in degrees. higher step size increase torque and accuracy.
+    int angle_max = 180;
+    int angle_min = 0;
 
 };

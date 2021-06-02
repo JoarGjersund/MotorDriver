bf5c9540f46aec345c0cb0d997dc83e5de33c1c5
Joar andreas Gjersund <joarandreasgjersund@Joars-Air.net>
2021-06-02 14:54:48 +0200

    changed to work with encoder

--- src/Motordriver.cpp
+++ src/Motordriver.cpp
@@ -1,7 +1,7 @@
 #include <Motordriver.h>
 
 
-MotorDriver::MotorDriver(int pin_enable, int pin_phase){
+MotorDriver::MotorDriver(double gearfactor, int pin_enable, int pin_phase){
 
     PIN_ENABLE=pin_enable;
     PIN_PHASE=pin_phase;
@@ -11,130 +11,97 @@ MotorDriver::MotorDriver(int pin_enable, int pin_phase){
 
 
 }
-bool MotorDriver::setCalibrationConstant(int newConstant){
-    calibrationConstant=newConstant;
-    return true;
-}
+void MotorDriver::setFrequency(float frequency) {
+    double newOffset=(_frequency-frequency)*millis()/1000+_phase_offset; // to make a smooth transition to new frequency we need to shift phase.
+    _phase_offset=newOffset;
 
-void MotorDriver::setMinimumStepSize(int newMinimumStepSize){
-    minimumStepSize=newMinimumStepSize;
-    return;
-}
-
-
-int MotorDriver::getCurrentPosition(){
-    return currentPosition;
-}
-
-int MotorDriver::getTargetPositionRounded(){
-
-    return round(targetPosition/minimumStepSize)*minimumStepSize;
+    _frequency=frequency;
 
 }
 
-bool MotorDriver::isMoving(){
+void MotorDriver::setAmplitude(float amplitude) {
 
-    return abs(currentPosition-getTargetPositionRounded()) >=  minimumStepSize;
-
-}
-
-
-
-
-void MotorDriver::recalibrate(int calibrationdirection, int calibrationspeed, int calibrationconstant_new, int percent_delay){
-
-    digitalWrite(PIN_PHASE, calibrationdirection);
-    analogWrite(PIN_ENABLE, calibrationspeed);
+    double ratio = _angle / amplitude;
+    if (abs(ratio) > 1)
+        _amplitude=_angle;
+    else
+        _amplitude=amplitude;
     
-
-    if (calibrationdirection==0) currentPosition=angle_min;
-    else currentPosition=angle_max;
-
-    if (calibrationconstant_new>0) calibrationConstant = calibrationconstant_new;
-    
-    unsigned int delayTime = round((percent_delay*(1/100.0)*calibrationConstant*(255.0/speed)));
-
-    long unsigned int start = millis();
-    while (millis()-start < delayTime){ }
-    analogWrite(PIN_ENABLE, 0);
-    timeWhenLastCalibrated = millis();
+    setOffset(); // to change amplitude smothly we need to shift phase so both angles match up.
 }
 
+void MotorDriver::stop(unsgined int delaytime_ms) {
+    _stop_t0=millis()+delaytime_ms;
 
-
-void MotorDriver::setTargetPosition(int target){
-    targetPosition=target;
+}
+void MotorDriver::pause(bool doPause) {
+    _pause=doPause;
 
 }
 
+double MotorDriver::setOffset() {
+    _phase_offset = asin(_angle/_amplitude) - _frequency * millis() / 1000; 
+}
 
-
-
-
-
-
-int MotorDriver::update(){
-
-
-    static bool init = false;
-    if (!init){
-        timeWhenLastUpdated=millis();
-        init=true;
+bool MotorDriver::update(int encoder_position) {
+    if (millis() < _stop_t0 || _pause){
+        setOffset();
+        return false;
     }
+    _angle = _amplitude*sin(_frequency*millis()/1000+_phase_offset); // asin(ft+ph)
 
-    int timeSinceLastUpdated=millis()-timeWhenLastUpdated;
-    
+    bool motor_move = true;
+    int16_t offset =  gearfactor*_angle - encoder_position;
+    int16_t output_voltage = 0;
+
+    // ramp up voltage until target is reached. ramp down voltage if target is reached.
+    if (offset > 0) output_voltage=torque;
+    else if (offset < 0) output_voltage=-torque;
+    else output_voltage=0;
 
 
-    int degreesMovedSinceLastUpdate=timeSinceLastUpdated*180/(calibrationConstant);
-
-    if (degreesMovedSinceLastUpdate>0) timeWhenLastUpdated=millis();
-
-    currentPosition+=degreesMovedSinceLastUpdate*direction;
-
-    int targetPositionRounded=round(targetPosition/minimumStepSize)*minimumStepSize;
-
-        // decide direction to move.
-    if ( targetPositionRounded > currentPosition && currentPosition < angle_max){
-        direction=1;
-        digitalWrite(PIN_PHASE, 255); 
-    } else if ( targetPositionRounded < currentPosition && currentPosition > angle_min){
-        direction=-1;
-        digitalWrite(PIN_PHASE, 0);
-    }
-
-    if (abs(currentPosition-targetPositionRounded) <  minimumStepSize || currentPosition > angle_max || currentPosition < angle_min )
+    // if max voltage and motor is still not moving assume stalling and cut voltage until moving the other way.
+    if (!isStalling && encoder_position == encoder_position_prev && abs(output_voltage) == torque)
     {
-        direction=0;
-        analogWrite(PIN_ENABLE, 0);
-    }else{
-        analogWrite(PIN_ENABLE, speed);
-    }
-
-    
-
-
-    if (millis()-timeWhenLastCalibrated >5000){
-        if (currentPosition>=angle_max) recalibrate(0, 255, calibrationConstant, 5);
-        else if (currentPosition<=angle_min) recalibrate(255, 255, calibrationConstant, 5);    
-    } else if (millis()-timeWhenLastCalibrated >10000){
-        if (currentPosition>(2*angle_max-angle_min)/3) recalibrate(0, 255, calibrationConstant, 10);
-        else if (currentPosition<(angle_max-angle_min)/3) recalibrate(255, 255, calibrationConstant, 10);        
-    } else if (millis()-timeWhenLastCalibrated >60000){
-        if (currentPosition>(5*angle_max-angle_min)/6) recalibrate(0, 255, calibrationConstant, 10);
-        else if (currentPosition<5*(angle_max-angle_min)/12) recalibrate(255, 255, calibrationConstant, 10);        
-    } else if (millis()-timeWhenLastCalibrated >120000){
-        if (currentPosition>(angle_max-angle_min)/2) recalibrate(0, 255, calibrationConstant, 10);
-        else recalibrate(255, 255, calibrationConstant, 10);        
-    }
-    
-
-    return currentPosition;
-
-
+        _angle_prev=_angle;
+        isStalling=true;
+        _stall_t0=millis()+stall_cutoff_ms;
 
     }
+    else if (encoder_position != encoder_position_prev) {
+        isStalling=false;
+    }
+    else if (isStalling && millis() > _stall_t0){
+        
+        output_voltage=0;
+        if (abs(_angle) <= abs(_angle_prev)){ 
+            isStalling=false;
+        }
+    }
 
+    // output direction and voltage to motor
+    digitalWrite(A_PH, (output_voltage > 0));
+    analogWrite(A_EN, abs(output_voltage));
+
+    // return true if motor has moved since last update
+    if (encoder_position == encoder_position_prev) motor_move=false;
+    else
+        motor_move=true;
+
+    encoder_position_prev = encoder_position;
+
+    return motor_move;
+
+}
+
+bool MotorDriver::isAtPeakTop() {
+
+
+}
+
+bool MotorDriver::isAtPeakBottom() {
+
+}
 
 
 
--- src/Motordriver.h
+++ src/Motordriver.h
@@ -4,31 +4,34 @@
 #endif
 class MotorDriver {
     public:
-    float phase = 0;
-    int amplitude = 0;
-    int offset = 90;
-    MotorDriver(int PIN_ENABLE, int PIN_PHASE);
-    int update();
-    void setTargetPosition(int target);
-    int getCurrentPosition();
-    bool setCalibrationConstant(int newConstant);
-    void setMinimumStepSize(int newMinimumStepSize);
-    int getTargetPositionRounded();
-    bool isMoving();
-    int targetPosition;
-    int speed = 255;
-    void recalibrate(int direction = 0, int speed = 255, int calibrationconstant_new = 0, int percent_delay = 100);
+    MotorDriver(int pin_enable, int pin_phase) // gearfactor: encoder_position = output_angle*gearfactor 
+    
+    double gearfactor = 430.0f / 360.0f; ;
+    void setFrequency(float frequency);
+    void setAmplitude(float amplitude); // current motor angle must be smaller than new amplitude
+    void stop(unsigned int delaytime_ms=0) // 
+    void pause(bool doPause);
+    bool update(int encoder_position); // returns true if motor is moving.
+    bool isAtPeakTop(); // for debounce purpose: calling this will clear flag until after peak bottom is reached
+    bool isAtPeakBottom(); // for debounce purpose: calling this will clear flag until after peak top is reached
 
     private:
-    int currentPosition = 0;
-    unsigned long timeWhenLastUpdated = 0;
-    unsigned long timeWhenLastCalibrated = 0;
-    int PIN_ENABLE;
-    int PIN_PHASE;
-    int direction = 0;
-    int calibrationConstant=130; // slower motor -> Higher number. ms needed to go from 0 to 180 at maximum speed.
-    int minimumStepSize=8; // minimum step size in degrees. higher step size increase torque and accuracy. should be defined by how often  m1.update() is called.
-    int angle_max = 180;
-    int angle_min = 0;
+    const int PIN_ENABLE;
+    const int PIN_PHASE;
+    float _frequency = 0.1;
+    float _amplitude = 10;
+    unsigned long _stop_t0 = 0;
+    unsigned long _stall_t0 = 0;
+    bool _pause = false;
+    float _angle = 0;
+    float _angle_prev = 0;
+    int16_t torque = 170;
+    int16_t torque = 170;
+    unsigned int stall_cutoff_ms = 50;
+    bool isStalling = false;
+    int encoder_position_prev = 0;
+    double _phase_offset = 0.0;
+    double setOffset();
+    
 
 };

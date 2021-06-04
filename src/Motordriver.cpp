217901eb1f0d4052ebac13311d5897212da0153e
Joar andreas Gjersund <joarandreasgjersund@Joars-Air.net>
2021-06-04 10:48:48 +0200

    more fixes. working good

--- src/Motordriver.cpp
+++ src/Motordriver.cpp
@@ -18,27 +18,34 @@ void MotorDriver::init() {
 }
 void MotorDriver::setFrequency(float frequency) {
     if (2*M_PI*frequency == _frequency) return;
-    frequency*=2*M_PI;
-    double newOffset=(_frequency-frequency)*millis()/1000.0+_phase_offset; // to make a smooth transition to new frequency we need to shift phase.
-    _phase_offset=newOffset;
-
-    _frequency=frequency;
+    newFrequency=frequency*2*M_PI;
 
 }
 
 void MotorDriver::setAmplitude(float amplitude) {
-    if (amplitude==newAmplitude || amplitude==_amplitude) return;
+    if (amplitude==_amplitude) return;
     newAmplitude=amplitude;
+}
+
+void MotorDriver::setPhaseOffset(float offset) {
+    _phase_offset+=offset;
 }
 
 void MotorDriver::stop(unsigned long delaytime_ms) {
     if (millis() <= _stop_t0) return;
     _stop_t0=millis()+delaytime_ms;
-    _phase_offset-=_frequency*delaytime_ms/1000.0;
+    _phase_offset-=_frequency*delaytime_ms/(_amplitude*1000.0);
 
 }
 void MotorDriver::pause(bool doPause) {
     _pause=doPause;
+
+}
+
+void MotorDriver::goTo(bool enable, float targetAngle, float acceleration) {
+    goAndStop=enable;
+    goAndStop_targetAngle=targetAngle;
+    goAndStop_acceleration=acceleration;
 
 }
 
@@ -49,44 +56,81 @@ bool MotorDriver::update(int encoder_position) {
         analogWrite(pin_en, 0);
         return false;
     }
+    if (goAndStop) {
+        
+        if ( abs(goAndStop_targetAngle*gearfactor-encoder_position) < 2){
+            analogWrite(pin_en, 0);
+            goAndStop_speed=0;
 
-    _angle_prev=_angle;
+        } else if (goAndStop_targetAngle*gearfactor > encoder_position){
+            goAndStop_speed+=goAndStop_acceleration;
+            if (goAndStop_speed>torque) goAndStop_speed=torque;
+            analogWrite(pin_en, goAndStop_speed);
+            digitalWrite(pin_ph, HIGH);
+
+        } else if (goAndStop_targetAngle*gearfactor < encoder_position){
+            goAndStop_speed+=goAndStop_acceleration;
+            if (goAndStop_speed>torque) goAndStop_speed=torque;
+            analogWrite(pin_en, goAndStop_speed);
+            digitalWrite(pin_ph, LOW);
+
+        }
+        return true;
+    }
+
+    
     // if we want to  change amplitude we do it when angle is 0 to avoid having to deal with phase change.
-    if (newAmplitude!=0 && round(_angle) == 0){
+    if (newAmplitude!=0 && floor(_angle*10)/10 == 0){
+        _phase_offset+=_frequency*(millis()/1000.0)*(1/_amplitude - 1/newAmplitude);
         _amplitude=newAmplitude;
         newAmplitude=0;
+        
+        
     }
-    _angle = _amplitude*sin(_frequency*millis()/1000.0+_phase_offset); // asin(ft+ph)
+    // we change frequency when want to do it at the paks. once angle is decreasing and we are at positive side of the axis we have just passed the top.
+    if (floor(_angle*10)/10 == _amplitude && newFrequency!=0) {
+        _frequency=newFrequency;
+        newFrequency=0;
+        _phase_offset=M_PI/2.0-_frequency*millis()/(_amplitude*1000.0);
+    }
+    // once angle is increasing and we are at negative side of the axis we have just passed the bottom.
+    if (floor(_angle*10)/10 == -_amplitude && newFrequency!=0) {
+        _frequency=newFrequency;
+        newFrequency=0;
+        _phase_offset=-M_PI/2.0-_frequency*millis()/(_amplitude*1000.0);
+    }
+    _angle_prev=_angle;
+    _angle = _amplitude*sin(_frequency*millis()/(_amplitude*1000.0)+_phase_offset); // asin(ft+ph)
 
     bool motor_move = true;
     int16_t offset =  gearfactor*_angle - encoder_position;
     int16_t output_voltage = 0;
 
     // ramp up voltage until target is reached. ramp down voltage if target is reached.
+    
     if (offset > 0) output_voltage=torque;
     else if (offset < 0) output_voltage=-torque;
     else output_voltage=0;
 
-
     // if max voltage and motor is still not moving assume stalling and cut voltage until moving the other way.
-    if (!isStalling && encoder_position == encoder_position_prev && abs(output_voltage) == torque)
+    if (!isStalling && encoder_position == encoder_position_prev && output_voltage!=0)
     {
-        _angle_prev=_angle;
+        _angle_stallStart=_angle;
         isStalling=true;
-        _stall_t0=millis()+stall_cutoff_ms;
+        _stall_t0=millis();
 
     }
     else if (encoder_position != encoder_position_prev) {
         isStalling=false;
     }
-    else if (isStalling && millis() > _stall_t0){
+    else if (isStalling && millis()-_stall_t0 > stall_cutoff_ms){
         
         output_voltage=0;
-        if (abs(_angle) <= abs(_angle_prev)){ 
+        if (abs(_angle) <= abs(_angle_stallStart)){ 
             isStalling=false;
         }
     }
-
+    output_voltage=round(output_voltage*(0.2*(millis()-_stall_t0)/stall_cutoff_ms +0.8));
     // output direction and voltage to motor
     digitalWrite(pin_ph, (output_voltage > 0));
     analogWrite(pin_en, abs(output_voltage));
@@ -103,7 +147,7 @@ bool MotorDriver::update(int encoder_position) {
 }
 
 bool MotorDriver::isAtPeakTop() {
-    if (isClimbing && _angle-_angle_prev < 0)
+    if (isClimbing && floor(_angle*10)/10-floor(_angle_prev*10)/10 < 0)
     {
         isClimbing=false;
         return true;
@@ -113,7 +157,7 @@ bool MotorDriver::isAtPeakTop() {
 
 bool MotorDriver::isAtPeakBottom() {
 
-    if (!isClimbing && _angle-_angle_prev > 0) {
+    if (!isClimbing && floor(_angle*10)/10-floor(_angle_prev*10)/10 > 0) {
         isClimbing=true;
         return true;
     }
--- src/Motordriver.h
+++ src/Motordriver.h
@@ -6,32 +6,42 @@ class MotorDriver {
     public:
     MotorDriver(int pin_enable, int pin_phase); 
     
-    double gearfactor = 5; //430.0f / 360.0f;// gearfactor: encoder_position = output_angle*gearfactor 
-    void setFrequency(float frequency); // Frequency in revolutions per second.
+    double gearfactor = 5; //430.0 / 360.0;// gearfactor: encoder_position = output_angle*gearfactor 
+    void setFrequency(float frequency); // Frequency in revolutions per second. Normalized to amplitude [ A*sin(f*t/A) ]
     void setAmplitude(float amplitude); // current motor angle must be smaller than new amplitude
+    void setPhaseOffset(float offset); // set offset in radians. will be added to current offset.
     void stop(unsigned long delaytime_ms=0); // 
     void pause(bool doPause);
     bool update(int encoder_position); // returns true if motor is moving.
     bool isAtPeakTop(); // for debounce purpose: calling this will clear flag until after peak bottom is reached
     bool isAtPeakBottom(); // for debounce purpose: calling this will clear flag until after peak top is reached
     void init();
+    void goTo(bool enable, float targetAngle, float acceleration); // set desired position and go.
     private:
     int pin_en;
     int pin_ph;
-    float _frequency = 0.1;
+    float _frequency = 6*M_PI;
     float _amplitude = 10;
     unsigned long _stop_t0 = 0;
     unsigned long _stall_t0 = 0;
     bool _pause = false;
     float _angle = 0;
     float _angle_prev = 0;
-    int16_t torque = 170;
-    unsigned int stall_cutoff_ms = 500;
+    float _angle_stallStart = 0;
+    
+    int16_t torque = 250;
+    unsigned int stall_cutoff_ms = 50;
     bool isStalling = false;
     int encoder_position_prev = 0;
     double _phase_offset = 0.0;
     bool isClimbing=false;
-    float newAmplitude=0;
+    float newAmplitude=0.0f;
+    float newFrequency=0.0f;
+    bool goAndStop=false;
+    float goAndStop_targetAngle=0;
+    double goAndStop_acceleration=0.1;
+    double goAndStop_speed=0.0;
+    int goAndStop_directionUp=0; // -1 down, +1 up, 0 nowhere.
     
 
 };
